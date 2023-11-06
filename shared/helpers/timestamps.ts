e56562c090033cc840108b5d57e8a9d2c1379209
Eve Aviv Keinan <evavkein@gmail.com>
2023-11-06 16:07:37 +0200

    feat (shared - helpers): timestamps.ts

--- shared/helpers/timestamps.ts
+++ shared/helpers/timestamps.ts
@@ -1,7 +1,7 @@
-export function unix() {
-	return Math.floor(Date.now() / (process.env.PROD ? 1000 : 1)); // conditional is for automated tests (to verify they get updated)
+export function unix(stamp?: Date | string) {
+	return Math.floor((stamp ? new Date(stamp).getTime() : Date.now()) / (process.env.PROD ? 1000 : 1)); // conditional is for automated tests (to verify they get updated)
 }
 
-export function iso() {
-	return new Date().toISOString();
+export function iso(stamp?: Date | string) {
+	return (stamp ? new Date(stamp) : new Date()).toISOString();
 }
