06add1b380e3b9edb975deeb341eb3b0512c03cc
Eve Aviv Keinan <evavkein@gmail.com>
2023-11-07 12:05:39 +0200

    refactor: remove tailwindcss
    gave tailwind an honest try, and i just don't like using it (or think it's a good idea)

--- frontend/src/pages/home.page.tsx
+++ frontend/src/pages/home.page.tsx
@@ -1,7 +1,9 @@
+import "./home.module.css";
+
 export default function Home() {
 	return (
 		<>
-			<h1 className={"text-center text-6xl font-extrabold"}>DialPlan</h1>
+			<h1>DialPlan</h1>
 		</>
 	);
 }
