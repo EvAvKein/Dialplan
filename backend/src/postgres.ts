814b2b0d8657b857dce7d5f31b57d9e9017c0a2e
Eve Aviv Keinan <evavkein@gmail.com>
2023-11-17 18:17:46 +0200

    refactor (postgres.ts): add pg-promise setup & export

--- backend/src/postgres.ts
+++ backend/src/postgres.ts
@@ -1,17 +1,22 @@
 import {readFileSync} from "fs";
 import pg from "pg"; // can't destructure because it's commonjs
+import pgp from "pg-promise";
 
-const postgres = new pg.Pool({
+const pgConfig = {
 	host: "postgres",
 	database: process.env.POSTGRES_USER,
 	user: process.env.POSTGRES_USER,
 	password: process.env.POSTGRES_PASSWORD,
-});
+} as const;
+
+const postgres = new pg.Pool(pgConfig);
 await postgres.connect().catch(() => console.log("Failed to connect to database"));
+type pgpPool = pgp.IDatabase<{poolConfig: never}>;
+const postgresP: pgpPool = pgp()(pgConfig);
 
 for (const fileName of ["org.sql", "inv.sql"]) {
 	const fileContent = readFileSync("/backend/sql/" + fileName, "utf-8");
 	postgres.query(fileContent).catch((error) => console.log(error));
 }
 
-export {postgres};
+export {postgres, postgresP, type pgpPool};
