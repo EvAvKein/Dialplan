98d17e52a484a8282085e3a3757bd27d7e0281d6
Eve Aviv Keinan <evavkein@gmail.com>
2023-11-24 21:06:56 +0200

    refactor (signUp.page.tsx): implement various improvements

--- frontend/src/pages/signUp/signUp.page.tsx
+++ frontend/src/pages/signUp/signUp.page.tsx
@@ -1,11 +1,20 @@
 import {OrgCreationRequest, AgentCreationRequest} from "../../../../shared/objects/org";
 import {apiFetch} from "../../helpers/apiFetch";
 import {useState} from "react";
-import {Input} from "../../components/inputs";
+import {Input, SearchableInput} from "../../components/inputs";
+import {timezones, timezoneValuesRegex} from "../../helpers/timezones";
+import {useNotifStore} from "../../stores/notifs";
+import coreStyles from "../../core.module.css";
 import styles from "./signUp.module.css";
 
 export default function SignUp() {
-	const [org, setOrg] = useState<OrgCreationRequest>({name: "", color: "", timezone: ""});
+	const notifs = useNotifStore();
+
+	const [org, setOrg] = useState<OrgCreationRequest>({
+		name: "",
+		color: getComputedStyle(document.body).getPropertyValue("--backgroundColor"),
+		timezone: Intl.DateTimeFormat().resolvedOptions().timeZone,
+	});
 	const [agent, setAgent] = useState<AgentCreationRequest>({
 		name: "",
 		department: "",
@@ -20,23 +29,43 @@ export default function SignUp() {
 				onSubmit={async (e) => {
 					e.preventDefault();
 					const response = await apiFetch("POST", "/orgs", {org, agent});
-					window.alert(response.error ? response.error.message : "Success!"); // just for testing ofc
+					notifs.create({
+						text: response.error?.message ?? "Success!",
+						desirability: !response.error,
+						manualDismiss: true,
+					});
 				}}
 			>
 				<section id="org">
 					<h3>Organization data</h3>
 					<Input placeholder="Name" handler={(val) => setOrg({...org, name: val})} />
 					<Input placeholder="Color" handler={(val) => setOrg({...org, color: val})} />
-					<Input placeholder="Timezone" handler={(val) => setOrg({...org, timezone: val})} />
+					<SearchableInput
+						id={"timezoneInput"}
+						type={"search"}
+						label={"Timezone"}
+						pattern={timezoneValuesRegex} // has hilariously large HTML output, but i think it's worth it
+						handler={(zone) => {
+							try {
+								new Date().toLocaleString([], {timeZone: zone});
+							} catch (err) {
+								return;
+							}
+							setOrg({...org, timezone: zone});
+						}}
+						options={timezones}
+					/>
 				</section>
 
 				<section id="agent">
-					<h3>Your agent data</h3>
+					<h3>Your personnel data</h3>
 					<Input placeholder="Name" handler={(val) => setAgent({...agent, name: val})} />
 					<Input placeholder="Department" handler={(val) => setAgent({...agent, department: val})} />
 					<Input placeholder="Country Code" handler={(val) => setAgent({...agent, countryCode: val})} />
 				</section>
-				<button type="submit">Submit</button>
+				<button className={coreStyles.backgroundButton} type="submit">
+					Submit
+				</button>
 			</form>
 			<section id="invitePreview">
 				{/* will be replaced with the same component as the recipient-view invite, for preview during registration */}
