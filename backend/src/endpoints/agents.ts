784621758818f7a71b487c0456addc901bb306dd
Eve Aviv Keinan <evavkein@gmail.com>
2023-12-18 17:45:27 +0200

    fix (backend): properly name /agents endpoints function

--- backend/src/endpoints/agents.ts
+++ backend/src/endpoints/agents.ts
@@ -4,7 +4,7 @@ import {FetchResponse} from "../../../shared/objects/api.js";
 // import { type Pool } from "pg";
 // import { type pgpPool } from "../postgres";
 
-export function endpoints_orgs(app: Express /*db: Pool, dbP: pgpPool*/) {
+export function endpoints_agents(app: Express /*db: Pool, dbP: pgpPool*/) {
 	app.get("/api/agents", async (request, response) => {
 		// TODO: add functionality for getting all users in own org
 		const agent = await authAgent(request);
