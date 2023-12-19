9adcae7f7d9f8f7722f30524b072db523ce209f8
Eve Aviv Keinan <evavkein@gmail.com>
2023-12-19 18:18:15 +0200

    fix (validationRegex): resolve frontend pattern attribute error

--- shared/objects/validationRegex.ts
+++ shared/objects/validationRegex.ts
@@ -1,3 +1,5 @@
+/* eslint-disable no-useless-escape */ // for escaping the dash symbol in regex, because otherwise inputs' pattern attribute breaks (at least chrome) with a JS error claiming it's invalid regex
+
 import {timezoneValues} from "./timezones.js";
 
 export const shared = {
@@ -7,13 +9,13 @@ export const shared = {
 };
 
 export const org = {
-	name: /^[\w -]{1,30}$/iu,
+	name: /^[\w \-]{1,30}$/iu,
 	color: /^[0-9A-F]{6}$/i,
 	timezone: shared.timezone,
 } as const;
 
 export const agent = {
-	name: /^[\w -]{1,30}$/iu, // max is not large enough for some names, but i'll make it clear that this is the name that'll appear on invites and should therefore be presentable. but also that approach might be too strict... i'll just have to see if using the unabbreviated versions of long names is that important to their cultures, even in business contexts, to be worth the design consequences
-	department: /^[\w -]{2,50}$/iu, // same as above, max length is subject to change
+	name: /^[\w \-]{1,30}$/iu, // max is not large enough for some names, but i'll make it clear that this is the name that'll appear on invites and should therefore be presentable. but also that approach might be too strict... i'll just have to see if using the unabbreviated versions of long names is that important to their cultures, even in business contexts, to be worth the design consequences
+	department: /^[\w \-]{2,50}$/iu, // same as above, max length is subject to change
 	countryCode: shared.countryCode,
 } as const;
