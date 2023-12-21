eabec9b325f8fad51b058fd1dd7feb58b1aab140
Eve Aviv Keinan <evavkein@gmail.com>
2023-12-21 20:16:03 +0200

    fix (validationRegex.ts): multiple fixes (see desc)
    - moved color regex's case-insensitivity from flag to pattern (and added comment above regarding regex flags)
    - removed unnecessary case-insensitivity flag from regexes that include \w (reduntant)
    - adjusted regexes which accept spaces to disallow leading & trailing spaces

--- shared/objects/validationRegex.ts
+++ shared/objects/validationRegex.ts
@@ -1,6 +1,7 @@
 /* eslint-disable no-useless-escape */ // for escaping the dash symbol in regex, because otherwise inputs' pattern attribute breaks (at least chrome) with a JS error claiming it's invalid regex
-
 import {timezoneValues} from "./timezones.js";
+
+// REMINDER: avoid flags for what's solvable with a pattern (except /u, which is applied automatically in frontend). these regexes are used for frontend inputs' "pattern" attribute, which doesn't support flags
 
 export const shared = {
 	phoneNumber: /^[1-9][0-9]{8}$/,
@@ -9,13 +10,13 @@ export const shared = {
 };
 
 export const org = {
-	name: /^[\w \-]{1,30}$/iu,
-	color: /^[0-9A-F]{6}$/i,
+	name: /^(?!\s)[\w \-]{1,30}(?<!\s)$/u,
+	color: /^[0-9a-fA-F]{6}$/,
 	timezone: shared.timezone,
 } as const;
 
 export const agent = {
-	name: /^[\w \-]{1,30}$/iu, // max is not large enough for some names, but i'll make it clear that this is the name that'll appear on invites and should therefore be presentable. but also that approach might be too strict... i'll just have to see if using the unabbreviated versions of long names is that important to their cultures, even in business contexts, to be worth the design consequences
-	department: /^[\w \-]{2,50}$/iu, // same as above, max length is subject to change
+	name: /^(?!\s)[\w \-]{1,30}(?<!\s)$/u, // max is not large enough for some names, but i'll make it clear that this is the name that'll appear on invites and should therefore be presentable. but also that approach might be too strict... i'll just have to see if using the unabbreviated versions of long names is that important to their cultures, even in business contexts, to be worth the design consequences
+	department: /^(?!\s)[\w \-]{2,50}(?<!\s)$/u, // same as above, max length is subject to change
 	countryCode: shared.countryCode,
 } as const;
