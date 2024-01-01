f9e4ef1927e2683b11ea1af9398be527963ed77b
Eve Aviv Keinan <evavkein@gmail.com>
2024-01-01 19:01:14 +0200

    fix: retry postgres connection in non-dev modes

--- backend/src/postgres.ts
+++ backend/src/postgres.ts
@@ -2,6 +2,7 @@ import {readFileSync} from "fs";
 import pg from "pg"; // can't destructure because it's commonjs
 import pgp from "pg-promise";
 
+const maxConnectionAttempts = 15;
 const pgConfig = {
 	host: "postgres",
 	database: process.env.POSTGRES_USER,
@@ -10,7 +11,21 @@ const pgConfig = {
 } as const;
 
 const postgres = new pg.Pool(pgConfig);
-await postgres.connect().catch(() => console.log("Failed to connect to database"));
+for (let attempt = 1; attempt < maxConnectionAttempts; attempt++) {
+	// very first setup from the postgres docker image takes a bit. without these retries, the non-dev version ends up with a borked db connection & setup
+	// TODO: improve readability
+	try {
+		await postgres.connect();
+		break;
+	} catch {
+		await new Promise((resolve) => setTimeout(resolve, 3000));
+	}
+
+	if (attempt > maxConnectionAttempts) {
+		throw new Error(`Failed to connect to postgres (${attempt}/${maxConnectionAttempts} attempts)`);
+	}
+}
+
 type pgpPool = pgp.IDatabase<{poolConfig: never}>;
 const postgresP: pgpPool = pgp()(pgConfig);
 
