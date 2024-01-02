8867a7e5edbace4d1fae4dce9ab6c030612847a9
Eve Aviv Keinan <evavkein@gmail.com>
2024-01-02 11:06:06 +0200

    fix (postgres): correct attempts check logic (& reduce limit)

--- backend/src/postgres.ts
+++ backend/src/postgres.ts
@@ -2,7 +2,7 @@ import {readFileSync} from "fs";
 import pg from "pg"; // can't destructure because it's commonjs
 import pgp from "pg-promise";
 
-const maxConnectionAttempts = 15;
+const maxConnectionAttempts = 10;
 const pgConfig = {
 	host: "postgres",
 	database: process.env.POSTGRES_USER,
@@ -11,7 +11,7 @@ const pgConfig = {
 } as const;
 
 const postgres = new pg.Pool(pgConfig);
-for (let attempt = 1; attempt < maxConnectionAttempts; attempt++) {
+for (let attempt = 1; attempt <= maxConnectionAttempts; attempt++) {
 	// very first setup from the postgres docker image takes a bit. without these retries, the non-dev version ends up with a borked db connection & setup
 	// TODO: improve readability
 	try {
@@ -22,7 +22,7 @@ for (let attempt = 1; attempt < maxConnectionAttempts; attempt++) {
 		await new Promise((resolve) => setTimeout(resolve, 3000));
 	}
 
-	if (attempt > maxConnectionAttempts) {
+	if (attempt === maxConnectionAttempts) {
 		throw new Error(`Failed to connect to postgres (${attempt}/${maxConnectionAttempts} attempts)`);
 	}
 }
