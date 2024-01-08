4f1f2c5e98ecf18db4c6515efe8d19ac7788709a
Eve Aviv Keinan <evavkein@gmail.com>
2024-01-08 13:59:03 +0200

    feat (backend - endpoints): create DELETE /sessions endpoint (replacing GET)
    replacing GET because there's no use for it yet, and i wanna reimplement it when it'll be necessary to practice my SQL

--- backend/src/endpoints/sessions.ts
+++ backend/src/endpoints/sessions.ts
@@ -1,37 +1,30 @@
 import {type Express} from "express";
 import {type Pool} from "pg";
-import {cookies as reqCookies} from "../../objects/cookies.js";
+import {type cookies, cookieKeys} from "../../../shared/objects/cookies.js";
 import {FetchResponse} from "../../../shared/objects/api.js";
 import {Agent} from "../../../shared/objects/org.js";
 
 export function endpoints_sessions(app: Express, db: Pool) {
-	app.get("/api/sessions", async (request, response) => {
-		const cookies: reqCookies = request.cookies;
-		if (!cookies.dialplan_agentid || !cookies.dialplan_sessionid) {
+	app.delete("/api/sessions", async (request, response) => {
+		const reqCookies: cookies = request.cookies;
+		if (!reqCookies.dialplan_agentid || !reqCookies.dialplan_sessionid) {
 			response.status(400).json(new FetchResponse(null, {message: "Missing authentication cookies"}));
 			return;
 		}
 
 		await db
 			.query<Agent>(
-				`WITH "authSession" AS (
-					SELECT *
-					FROM "AgentSession"
-					WHERE "sessionId" = $1 AND "agentId" = $2
-					LIMIT 1
-				)
-				SELECT *
+				`DELETE
 				FROM "AgentSession"
-				WHERE "agentId" = "authSession"."agentId"
-				LIMIT 1
+				WHERE "id" = $1 AND "agentId" = $2
 				`,
-				[],
+				[reqCookies.dialplan_sessionid, reqCookies.dialplan_agentid],
 			)
 			.then(() => {
-				response.status(200).end();
+				response.status(200).clearCookie(cookieKeys.sessionid).clearCookie(cookieKeys.agentid).end();
 			})
 			.catch((error) => {
-				console.error("Org creation failure", error);
+				console.error("Session deletion failure", error);
 				response.status(500).json(new FetchResponse(null, {message: "Database error"}));
 			});
 	});
