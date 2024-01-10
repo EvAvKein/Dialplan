54dcc536d5a4462eaf0f28477fd466d0b0a9c52e
Eve Aviv Keinan <evavkein@gmail.com>
2024-01-10 12:32:03 +0200

    feat (backend): create POST /sessions endpoint

--- backend/src/endpoints/sessions.ts
+++ backend/src/endpoints/sessions.ts
@@ -1,10 +1,48 @@
 import {type Express} from "express";
 import {type Pool} from "pg";
 import {type cookies, cookieKeys} from "../../../shared/objects/cookies.js";
+import {type Agent} from "../../../shared/objects/org.js";
 import {FetchResponse} from "../../../shared/objects/api.js";
-import {Agent} from "../../../shared/objects/org.js";
+import {v4 as uuid} from "uuid";
+import {tempSignInData} from "../validation/org.js";
+import {fromZodError} from "zod-validation-error";
 
 export function endpoints_sessions(app: Express, db: Pool) {
+	app.post("/api/sessions", async (request, response) => {
+		const signInData = tempSignInData.safeParse(request.body);
+		if (!signInData.success) {
+			response.status(400).json(new FetchResponse(null, {message: fromZodError(signInData.error).message}));
+			return;
+		}
+
+		const newSessionId = uuid();
+
+		await db
+			.query<Agent>(
+				`WITH agent AS (
+					SELECT "id"
+					FROM "Agents"
+					WHERE "id" = $1 AND "orgId" = $2
+				)
+				INSERT INTO "AgentSessions"
+				("id", "agentId")
+				VALUES $3, agent."id";
+				`,
+				[signInData.data.agentId, signInData.data.orgId, newSessionId],
+			)
+			.then(() => {
+				response
+					.status(200)
+					.cookie(cookieKeys.agentid, signInData.data.agentId)
+					.cookie(cookieKeys.sessionid, newSessionId)
+					.end();
+			})
+			.catch((error) => {
+				console.error("Session creation failure", error);
+				response.status(500).json(new FetchResponse(null, {message: "Database error"}));
+			});
+	});
+
 	app.delete("/api/sessions", async (request, response) => {
 		const reqCookies: cookies = request.cookies;
 		if (!reqCookies.dialplan_agentid || !reqCookies.dialplan_sessionid) {
