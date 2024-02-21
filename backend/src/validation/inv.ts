66e8c0431b5e3e676466073ce1bb9c72d6c9d594
Eve Aviv Keinan <evavkein@gmail.com>
2024-02-21 16:47:39 +0200

    fix (backend - validation): apply regex in InviteCreationRequest validation

--- backend/src/validation/inv.ts
+++ backend/src/validation/inv.ts
@@ -1,21 +1,25 @@
 import {z, type ZodSchema} from "zod";
 import * as classes from "../../../shared/objects/inv.js";
 import * as schemas from "./shared.js";
+import {invite} from "../../../shared/objects/validationRegex.js";
 
 const Recipient = z.object({
-	name: z.string().trim(),
-	phone: z.object({number: schemas.phoneNumber, countryCode: schemas.countryCode}),
+	name: z.string().trim().regex(invite.recipient.name),
+	phone: z.object({
+		number: z.string().trim().regex(invite.recipient.phone.number),
+		countryCode: z.string().trim().regex(invite.recipient.phone.countryCode),
+	}),
 }) satisfies ZodSchema<classes.CallRecipient>;
 
 const InviteNotes = z.object({
-	forRecipient: z.string().trim().optional(),
-	forOrg: z.string().trim().optional(),
+	forRecipient: z.string().trim().regex(invite.notes.forRecipient!).optional(),
+	forOrg: z.string().trim().regex(invite.notes.forOrg!).optional(),
 }) satisfies ZodSchema<classes.InviteNotes>;
 
 export const InviteCreationRequest = z.object({
 	recipient: Recipient,
-	callDuration: z.number().int().min(1),
-	expiry: schemas.timestamp,
+	callDuration: z.number().refine((val) => invite.callDuration.test(val.toString())),
+	expiry: z.string().trim().datetime({precision: 0}),
 	notes: InviteNotes,
 }) satisfies ZodSchema<classes.InviteCreationRequest>;
 
