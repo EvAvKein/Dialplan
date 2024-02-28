fd9fb7b70394743bc3fc68043fee6424ce1b6ac5
Eve Aviv Keinan <evavkein@gmail.com>
2024-02-28 18:19:19 +0200

    fix (frontend - secsToReadableDuration.ts): prevent hours higher than a day from being accumulated despite new day count
    ...plus some refactoring to improve readability

--- frontend/src/helpers/secsToReadableDuration.ts
+++ frontend/src/helpers/secsToReadableDuration.ts
@@ -1,10 +1,13 @@
-const secsInHour = 3600;
+const second = 1;
+const minute = 60 * second;
+const hour = 60 * minute;
+const day = 24 * hour;
 
 export function secsToReadableDuration(secs: number) {
-	const seconds = Math.floor(secs % 60);
-	const minutes = Math.floor((secs % secsInHour) / 60);
-	const hours = Math.floor(secs / secsInHour);
-	const days = Math.floor(secs / (secsInHour * 24));
+	const seconds = Math.floor(secs % minute);
+	const minutes = Math.floor((secs % hour) / minute);
+	const hours = Math.floor((secs % day) / hour);
+	const days = Math.floor(secs / day);
 
 	return `${days ? `${days}d ` : ""}${hours ? `${hours}h ` : ""}${minutes ? `${minutes}m ` : ""}${
 		seconds ? `${seconds}s` : ""
