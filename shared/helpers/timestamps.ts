3ff91e3cf101d0bb82b8181206da16f174f75a61
Eve Aviv Keinan <evavkein@gmail.com>
2024-03-01 16:47:32 +0200

    feat (shared - timestamps.ts): set timestamps' milliseconds to 0
    was already omitting milliseconds in production for security reasons, now decided to do that for all timestamps due to prospective unit tests (and the absent prospective need for millisecond accuracy)

--- shared/helpers/timestamps.ts
+++ shared/helpers/timestamps.ts
@@ -1,7 +1,9 @@
 export function unix(stamp?: Date | string) {
-	return Math.floor((stamp ? new Date(stamp).getTime() : Date.now()) / (process.env.PROD ? 1000 : 1)); // conditional is for automated tests (to verify they get updated)
+	return Math.floor((stamp ? new Date(stamp).getTime() : Date.now()) / 1000); // conditional is for automated tests (to verify they get updated)
 }
 
 export function iso(stamp?: Date | string) {
-	return (stamp ? new Date(stamp) : new Date()).toISOString();
+	const date = stamp ? new Date(stamp) : new Date();
+	date.setMilliseconds(0);
+	return date.toISOString();
 }
