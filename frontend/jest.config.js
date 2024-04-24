849c3cadbe7986b1de949646d6bdc15cbecb54cb
Eve Aviv Keinan <evavkein@gmail.com>
2024-04-24 20:11:51 +0300

    test (frontend): create mock for raw css imports

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -6,6 +6,7 @@ const config = {
 	testEnvironment: "jsdom",
 	moduleNameMapper: {
 		"\\.css$": "<rootDir>/src/mocks/cssModule.ts",
+		"\\.css\\?raw$": "<rootDir>/src/mocks/cssModule_raw.ts",
 	},
 };
 
